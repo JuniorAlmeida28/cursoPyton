d8597a053a75510d929e9f355bdb2882aa41bbdd
Junior Almeida <jrjunioralmeida@outlook.com>
2023-05-25 15:45:05 -0300

    Exercicios

---
+++ pyExercicios/Ex005.py
@@ -0,0 +1,5 @@
+num = int(input('Digite um número: '))
+ante = num - 1
+suce = num + 1
+print('O antecessor de {} é: {}'.format(num, ante))
+print('O sucessor de {} é: {}'.format(num, suce))
---
+++ pyExercicios/Ex006.py
@@ -0,0 +1,5 @@
+num = int(input('Digite um número: '))
+dobro = num * 2
+triplo = num * 3
+raiz = num ** (1/2)
+print('O dobro é: {} \nO triplo é: {} \nA raiz quadrada é: {}'.format(dobro, triplo, raiz))
